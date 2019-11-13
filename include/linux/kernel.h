dfa9e4767ce04c9671ced4ef67ec4f65c84502d7
Laura Abbott <labbott@redhat.com>
2019-11-13 14:44:31 -0500

    Add Red Hat tainting
    Message-id: <20191113144431.9403-5-labbott@redhat.com>
    Patchwork-id: 286078
    O-Subject: [ARK INTERNAL PATCHv2 4/4] [redhat] Add Red Hat tainting
    Bugzilla:
    RH-Acked-by: Don Zickus <dzickus@redhat.com>
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    This is a combination of the following commits including support
    for adding CONFIG_RH_DEPRECATED support
    
    rh_taint: add support
    
    Message-id: <1524831536-47239-1-git-send-email-darcari@redhat.com>
    Patchwork-id: 211655
    O-Subject: [RHEL8.0 BZ 1565704 v2] rh_taint: add support
    Bugzilla: 1565704
    RH-Acked-by: Jonathan Toppins <jtoppins@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    
    Bugzilla: http://bugzilla.redhat.com/1565704
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=15910441
    Upstream: RHEL Only
    Test: Sanity boot of brew build.  Previous private build testing of functions.
    
    rh_taint.c adds the following functions which are utilized by Red Hat to
    indicate limited or no support for hardware and/or drivers.
    
    mark_hardware_unsupported
    mark_hardware_deprecated
    mark_tech_preview
    
    See code comments for usage.
    
    RHEL8 change: TAINT_AUX will be used in place of TAINT_TECH_PREVIEW as it
    limits the number RHEL only changes that must be made to the kernel.
    
    If at a later date it is determined that additional flags are required,
    the associated RHEL only infrastructure can be added at that time.
    
    Cc: David Arcari <darcari@redhat.com>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>
    
    rh_taint: add support for marking driver as unsupported
    
    Message-id: <f42d35b07bd02f34b14a6341a219c80b21a8aff9.1524523802.git.jtoppins@redhat.com>
    Patchwork-id: 211020
    O-Subject: [RHEL8.0] rh_taint: add support for marking driver as unsupported
    Bugzilla: 1565704
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: David Arcari <darcari@redhat.com>
    
    In some cases a driver needs to be enabled by Red Hat to support internal
    testing but the driver is not intended to be supported on customer hardware.
    This adds a function "make_driver_unsupported()" to facilitate such
    situations. The function will print a kernel log message.
    
    Bugzilla: 1565704
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=15873552
    Tested: compile only
    
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>
    
    rh_taint: correct loaddable module support dependencies
    
    Message-id: <20181123111337.26898-5-prudo@redhat.com>
    Patchwork-id: 233658
    O-Subject: [ARK PATCH RESEND v2 4/8] [kernel] rh_taint: correct loaddable module support dependencies
    Bugzilla: 1652266
    RH-Acked-by: Jakub Racek <jracek@redhat.com>
    RH-Acked-by: Jiri Benc <jbenc@redhat.com>
    RH-Acked-by: Cornelia Huck <cohuck@redhat.com>
    
    Bugzilla: 1652266
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=19252944
    Upstream Status: RHEL_only
    
    The kernel configuration for the s390/zfcpdump kernel variant turns
    loaddable module support off.  The build breaks in rh_taint.c due to
    compiler errors caused by dereferencing struct module variables which
    is just a declaration if CONFIG_MODULES is not set.
    
    To solve this build break, access struct module members only if
    CONFIG_MODULES is enabled.
    
    Upstream Status: RHEL only
    Signed-off-by: Philipp Rudo <prudo@redhat.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -194,4 +194,31 @@ extern enum system_states system_state;
 # define REBUILD_DUE_TO_DYNAMIC_FTRACE
 #endif
 
+/* Permissions on a sysfs file: you didn't miss the 0 prefix did you? */
+#define VERIFY_OCTAL_PERMISSIONS(perms)						\
+	(BUILD_BUG_ON_ZERO((perms) < 0) +					\
+	 BUILD_BUG_ON_ZERO((perms) > 0777) +					\
+	 /* USER_READABLE >= GROUP_READABLE >= OTHER_READABLE */		\
+	 BUILD_BUG_ON_ZERO((((perms) >> 6) & 4) < (((perms) >> 3) & 4)) +	\
+	 BUILD_BUG_ON_ZERO((((perms) >> 3) & 4) < ((perms) & 4)) +		\
+	 /* USER_WRITABLE >= GROUP_WRITABLE */					\
+	 BUILD_BUG_ON_ZERO((((perms) >> 6) & 2) < (((perms) >> 3) & 2)) +	\
+	 /* OTHER_WRITABLE?  Generally considered a bad idea. */		\
+	 BUILD_BUG_ON_ZERO((perms) & 2) +					\
+	 (perms))
+
+struct module;
+
+#ifdef CONFIG_RH_DISABLE_DEPRECATED
+void mark_hardware_unsupported(const char *msg);
+void mark_hardware_deprecated(const char *msg);
+void mark_tech_preview(const char *msg, struct module *mod);
+void mark_driver_unsupported(const char *name);
+#else
+static inline void mark_hardware_unsupported(const char *msg) { }
+static inline void mark_hardware_deprecated(const char *msg) { }
+static inline void mark_tech_preview(const char *msg, struct module *mod) { }
+static inline void mark_driver_unsupported(const char *name) { }
+#endif
+
 #endif
