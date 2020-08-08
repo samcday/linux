61202b98c051a467f510637a3471be9f6b77800e
Jumar Macato <hikari.netto23@gmail.com>
2020-08-08 11:32:08 +0800

    update build script

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@
 
 set -e
 
+objcopy -I ihex -O binary firmware/imx/sdma/sdma-imx50.bin.ihex firmware/imx/sdma/sdma-imx50.bin
+
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx50_kindle4nt_defconfig
 
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j 8
