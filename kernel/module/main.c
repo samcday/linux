76684e826bf1f1003bce44d8bcf8dbe181b231e1
Ricardo Robaina <rrobaina@redhat.com>
2024-05-10 20:31:21 -0300

    rh_flags: Rename rh_features to rh_flags
    JIRA: https://issues.redhat.com/browse/RHEL-32987
    
    Conflicts:
       - Unrelated hunks dropped
       - Minor context differences due to 76106c6dac3 ('add support for rh_features')
         and d756b75a7f90 ('rh_features: move rh_features entry to sys/kernel')
    
    This patch is a backport of the following upstream commit:
    commit dd996f09078abf3d8a8ef1ac77616297bc77b598
    Author: Prarit Bhargava <prarit@redhat.com>
    Date:   Tue Nov 9 09:52:38 2021 -0500
    
        rh_flags: Rename rh_features to rh_flags
    
        Bugzilla: https://bugzilla.redhat.com/2021700
    
        Upstream Status: RHEL only
    
        Features implies that this code can only be used to indicate
        functionality, however, I'd like to also use these as debug flags.
    
        Rename rh_features to rh_flags, and change the output to indicate "Red
        Hat Flags" instead of "Features".
    
        Signed-off-by: Prarit Bhargava <prarit@redhat.com>
    
    Signed-off-by: Ricardo Robaina <rrobaina@redhat.com>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -66,7 +66,7 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/module.h>
 
-#include <linux/rh_features.h>
+#include <linux/rh_flags.h>
 
 /*
  * Mutex protects:
@@ -3985,7 +3985,7 @@ void print_modules(void)
 	pr_cont("\n");
 
 #ifdef CONFIG_RHEL_DIFFERENCES
-	rh_print_used_features();
+	rh_print_flags();
 #endif
 }
 
