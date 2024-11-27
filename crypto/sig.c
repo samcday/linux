a3eb4968dd241a5ea9a0ee49b4dda6a9a822cf07
Herbert Xu <herbert.xu@redhat.com>
2024-11-27 16:26:33 +0800

    crypto: sig - Disable signing
    Upstream Status: RHEL only
    
    This is a refresh of commit b4040234549b ("crypto: akcipher -
    Disable signing and decryption") with the upstream addition of
    the sig algorithm backend.
    
    It is otherwise identical to the original commit.
    
    Signed-off-by: Herbert Xu <herbert.xu@redhat.com>

--- crypto/sig.c
+++ crypto/sig.c
@@ -106,8 +106,7 @@ static int sig_prepare_alg(struct sig_alg *alg)
 {
 	struct crypto_alg *base = &alg->base;
 
-	if (!alg->sign)
-		alg->sign = sig_default_sign;
+	alg->sign = sig_default_sign;
 	if (!alg->verify)
 		alg->verify = sig_default_verify;
 	if (!alg->set_priv_key)
