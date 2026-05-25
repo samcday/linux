fe4ef1b11c50f2703db90274e5194c50dd61857b
Sam Day <me@samcday.com>
2026-05-25 14:36:08 +1000

    WIP: drm/panel: use non-continuous Teisko DSI clock

--- drivers/gpu/drm/panel/panel-nokia-teisko.c
+++ drivers/gpu/drm/panel/panel-nokia-teisko.c
@@ -325,7 +325,8 @@ static int teisko_probe(struct mipi_dsi_device *dsi)
 
 	dsi->lanes = 2;
 	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->mode_flags = MIPI_DSI_MODE_VIDEO;
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO |
+			  MIPI_DSI_CLOCK_NON_CONTINUOUS;
 
 	drm_panel_init(&ctx->panel, dev, &teisko_panel_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
