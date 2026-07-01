57UU/linux-sirius

media: venus: use a null pointer on HFI 4xx
On the Pixel 3a (SDM670), the Venus firmware tries to handle buffers
under Content Protection (CP) differently from other regular buffers,
according to each buffer's physical address. This doesn't work with
the hardcoded EOS buffer, 0xDEADB000. Use the null address (0) for
HFI 4xx.

A different implementation allocates a 4K buffer for sending an EOS, but
it is more complex than using a different pointer that works.

Sample error dmesg:

        [ 1889.304675] qcom-venus aa00000.video-codec: session error: event id:1001 (deadb000), session id:f6c9ac0d
        [ 1889.304699] qcom-venus-decoder aa00000.video-codec:video-core0: dec: event session error 1001

Error message from Venus firmware (HFI_MSG_SYS_DEBUG message):

        <VFW_E:HostDr:264d:f6c9ac0d:0> IsBufferProtected(1087): VenusCoreCtrl: CP_UNKNOWN, buffer straddles CP & non-CP regions

Signed-off-by: Richard Acayan <mailingradian@gmail.com>

drivers/media/platform/qcom/venus/vdec.c
@@ -569,7 +569,8 @@ vdec_decoder_cmd(struct file *file, void *fh, struct v4l2_decoder_cmd *cmd)
 
 		fdata.buffer_type = HFI_BUFFER_INPUT;
 		fdata.flags |= HFI_BUFFERFLAG_EOS;
-		if (IS_V6(inst->core) && is_fw_rev_or_older(inst->core, 1, 0, 87))
+		if ((IS_V6(inst->core) && is_fw_rev_or_older(inst->core, 1, 0, 87))
+		 || IS_V4(inst->core))
 			fdata.device_addr = 0;
 		else
 			fdata.device_addr = 0xdeadb000;

samcday/linux

drivers/media/platform/qcom/venus/vdec.c
@@ -575,7 +575,8 @@ vdec_decoder_cmd(struct file *file, void *fh, struct v4l2_decoder_cmd *cmd)
 		 * avoids misapplying this quirk and breaking VP9 decode on SC7280.
 		 */
 
-		if (IS_IRIS2(inst->core) && is_fw_rev_or_older(inst->core, 1, 0, 87))
+		if ((IS_IRIS2(inst->core) && is_fw_rev_or_older(inst->core, 1, 0, 87))
+		 || IS_V4(inst->core))
 			fdata.device_addr = 0;
 		else
 			fdata.device_addr = 0xdeadb000;